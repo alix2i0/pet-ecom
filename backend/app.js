843f758436524576d897c9174e19d05319860c02
mouyox21 <anibamouad@gmail.com>
2024-04-22 17:25:35 +0200

    "Update backend/app.js and add new files for routes in backend/routes/api.js"
    This commit includes changes to the backend/app.js file where additional middleware for CORS and the removal of some route imports

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 const express = require("express"); // Importing the Express framework
 const app = express(); // Creating an instance of the Express application
 require('./config/database'); // Importing database configuration
-const session = require('express-session'); // Importing express-session for session management
 const cookieParser = require('cookie-parser'); // Importing cookie-parser for parsing cookies
-const userRoutes = require('./routes/userRoutes'); // Importing user routes
-const authRoutes = require('./routes/authRoutes'); // Importing authentication routes
-const cmdRoutes = require('./routes/orderRoutes'); // Importing order routes
 
-const orderRoutes = require('./routes/orderRoutes');
-const apiRoutes = require('./routes/api');
+
+// Middleware to handle data parsing
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+    optionSuccessStatus: 200,
+    methods: ["GET", "HEAD", "PUT", "POST", "DELETE"],
+  }))
+
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cookieParser()); // Middleware for parsing cookies
@@ -17,23 +22,13 @@ app.use(cookieParser()); // Middleware for parsing cookies
 // Utilisez Passport middleware
 app.use(express.json());
 
-// // Routes
-
-app.use('/api/orders', orderRoutes);
-
-
-
 // Defining routes for authentication and user-related actions
-app.use('/auth', authRoutes);
-
-app.use('/user', userRoutes)
 app.use('/api', apiRoutes);
 
 
 
 app.use(express.urlencoded({ extended: true })); // Middleware for parsing URL-encoded bodies
 
-// Another route definition for user-related actions
-app.use("/api/users", userRoutes);
+
 
 module.exports = app; // Exporting the Express application
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const orderRoutes = require('./orderRoutes');
+const orderRoutes = require('./orderRoutes.js');
 const productRoutes = require('./productRoutes.js');
+const userRoutes = require("./userRoutes.js");
+const authRoutes  = require("./authRoutes.js")
+
+
 router.use('/products', productRoutes);
 router.use('/orders', orderRoutes);
+router.use("/users", userRoutes)
+router.use("/auth",authRoutes)
 
 module.exports = router;
