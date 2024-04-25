33865f88ffc53e73e5f5ea5a7692215ea02ef361
alix2i0 <alibounkhila@gmail.com>
2024-04-25 12:47:49 +0000

    test main

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
 const express = require("express"); // Importing the Express framework
 const app = express(); // Creating an instance of the Express application
 require('./config/database'); // Importing database configuration
-const session = require('express-session'); // Importing express-session for session management
 const cookieParser = require('cookie-parser'); // Importing cookie-parser for parsing cookies
-const userRoutes = require('./routes/userRoutes'); // Importing user routes
-const authRoutes = require('./routes/authRoutes'); // Importing authentication routes
-const cmdRoutes = require('./routes/orderRoutes'); // Importing order routes
-const cors = require('cors');
+const apiRoutes = require('./routes/api'); // Import
 
-const orderRoutes = require('./routes/orderRoutes');
-const apiRoutes = require('./routes/api');
+// Middleware to handle data parsing
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+    optionSuccessStatus: 200,
+    methods: ["GET", "HEAD", "PUT", "POST", "DELETE"],
+  }))
+
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cookieParser()); // Middleware for parsing cookies
 
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
-
-// // Routes
+// Defining routes for authentication and user-related actions
 app.use('/api', apiRoutes);
 
 app.use(express.urlencoded({ extended: true })); // Middleware for parsing URL-encoded bodies
+
 
 module.exports = app; // Exporting the Express application
